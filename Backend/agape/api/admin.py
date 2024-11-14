726d9b95a9537e0f9e4d0377cfb52d431627c282
= <=>
2024-11-14 23:39:24 +0300

    register models to django admin

--- Backend/agape/api/admin.py
+++ Backend/agape/api/admin.py
@@ -1,6 +1,16 @@
-# from django.contrib import admin
-# from .models import User, Warrant, DisabilityRecord
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# admin.site.register(User)
-# admin.site.register(Warrant)
-# admin.site.register(DisabilityRecord)
+
+from .models import User, Warrant, DisabilityRecord
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'first_name', 'last_name', 'email', 'role', 'gender', 'phone_number', 'is_staff']
+    search_fields = ['username', 'first_name', 'last_name', 'email']
+    ordering = ['username']
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Warrant)
+admin.site.register(DisabilityRecord)
