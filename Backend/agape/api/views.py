e4906144a87008ba5d61d8320ef40d401b2e32e2
= <=>
2024-11-16 23:48:51 +0300

    update user model

--- Backend/agape/api/views.py
+++ Backend/agape/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterUserSerializer
 
-# Create your views here.
+class RegisterUserView(APIView):
+    def post(self, request):
+        print('data is',request.data)
+        serializer = RegisterUserSerializer(data=request.data,partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
