fb07d5ebb5001ff18f536d9a1b27794a108bcce3
= <=>
2024-11-21 22:48:29 +0300

    authentication ,verification,reset password,verify otp implemented

--- Backend/agape/api/utils.py
+++ Backend/agape/api/utils.py
@@ -3,6 +3,10 @@ from django.core.mail import send_mail
 from django.conf import settings
 import logging
 
+import re
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
 logger = logging.getLogger(__name__)
 
 def send_email(subject, message, recipient_list):
@@ -18,3 +22,16 @@ def send_email(subject, message, recipient_list):
     except Exception as e:
         logger.error(f"Error sending email: {str(e)}")
         return None
+
+
+
+
+def validate_password(password):
+    if len(password) < 8:
+        raise ValidationError(_("Password must be at least 8 characters long."))
+
+    if " " in password:
+        raise ValidationError(_("Password should not contain spaces."))
+
+    # If all checks pass, return the password
+    return password
