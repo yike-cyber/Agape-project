becc2f7328989f9f4c2ab7301943349e41f94b7e
= <=>
2024-11-22 21:28:23 +0300

    update disability record model to add deleted field

--- Backend/agape/api/models.py
+++ Backend/agape/api/models.py
@@ -111,6 +111,7 @@ class DisabilityRecord(models.Model):
     is_provided = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.wheelchair_type}"
