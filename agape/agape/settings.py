c1afcba119429325131dbdf82d04c0a88a4072ff
= <=>
2024-11-23 20:17:02 +0300

    update requirements.txt

--- agape/agape/settings.py
+++ agape/agape/settings.py
@@ -113,10 +113,15 @@ WSGI_APPLICATION = 'agape.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'agape_db',
+        'USER': 'agape_user',
+        'PASSWORD': 'agape123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
