67d71160608cc89aa9a2ddf8cd340b6f0eda7e04
= <=>
2024-11-24 02:35:47 +0300

    add db info to the .venv

--- agape/api/models.py
+++ agape/api/models.py
@@ -54,6 +54,7 @@ class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    
     username = None
     USERNAME_FIELD = 'email'
     
