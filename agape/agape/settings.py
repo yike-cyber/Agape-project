35abb4c60f198d5e4032e34cf05d9825ed52848a
= <=>
2024-11-24 05:51:21 +0300

    make debut true

--- agape/agape/settings.py
+++ agape/agape/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG')
+DEBUG = True
 
 
 # Application definition
