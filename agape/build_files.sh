bae9e913768da286eddbee615702878b1f998eae
= <=>
2024-11-25 22:03:17 +0300

    add env variable ,secret key

--- agape/build_files.sh
+++ agape/build_files.sh
@@ -2,10 +2,9 @@
 
 echo "BUILD START"
 
-# Use Python 3.11 for all commands
-python3.9 -m pip install --upgrade pip  # Upgrade pip to avoid compatibility issues
-python3.9 -m pip install -r requirements.txt  # Install dependencies
-python3.9 manage.py collectstatic --noinput --clear  # Collect static files
+python3.9 -m pip install --upgrade pip  
+python3.9 -m pip install -r requirements.txt  
+python3.9 manage.py collectstatic --noinput --clear 
 python3.9 manage.py migrate --noinput  # Run migrations
 
 echo "BUILD END"
