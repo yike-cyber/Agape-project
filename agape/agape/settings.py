d1fd5ca2edf9ab1c0c5697997fa509a375ea2cbf
= <=>
2024-11-25 14:39:36 +0300

    remove build_files.sh file

--- agape/agape/settings.py
+++ agape/agape/settings.py
@@ -155,7 +155,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'api.User'
 
 
-ALLOWED_HOSTS = ['*.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app' ]
 
 CORS_ALLOW_ALL_ORIGINS = True 
 
