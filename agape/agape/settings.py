97649a0f10f735b783a6c4bbd19c9b11027ea7ab
= <=>
2024-11-25 11:35:55 +0300

    update vercel.json

--- agape/agape/settings.py
+++ agape/agape/settings.py
@@ -47,14 +47,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     
-  'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-    ],
-    'DEFAULT_PARSER_CLASSES': [
-         'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.JSONParser',
-        'rest_framework.parsers.MultiPartParser',
-    ],
+  
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
