b053dc50049d955a03160bdeb0fe17c3dea86cbf
= <=>
2024-11-25 21:44:50 +0300

    downgrade django to v 4.2 and python 3.9

--- agape/build_files.sh
+++ agape/build_files.sh
@@ -3,9 +3,9 @@
 echo "BUILD START"
 
 # Use Python 3.11 for all commands
-python3.11 -m pip install --upgrade pip  # Upgrade pip to avoid compatibility issues
-python3.11 -m pip install -r requirements.txt  # Install dependencies
-python3.11 manage.py collectstatic --noinput --clear  # Collect static files
-python3.11 manage.py migrate --noinput  # Run migrations
+python3.9 -m pip install --upgrade pip  # Upgrade pip to avoid compatibility issues
+python3.9 -m pip install -r requirements.txt  # Install dependencies
+python3.9 manage.py collectstatic --noinput --clear  # Collect static files
+python3.9 manage.py migrate --noinput  # Run migrations
 
 echo "BUILD END"
