f4d23d087bf946e563d53751660f573251de156b
= <=>
2024-11-25 20:00:05 +0300

    update buile_files.sh

--- agape/build_files.sh
+++ agape/build_files.sh
@@ -2,10 +2,10 @@
 
 echo "BUILD START"
 
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput --clear
-
-python3.11 manage.py migrate --noinput
-
+# Use Python 3.11 for all commands
+python3.11 -m pip install --upgrade pip  # Upgrade pip to avoid compatibility issues
+python3.11 -m pip install -r requirements.txt  # Install dependencies
+python3.11 manage.py collectstatic --noinput --clear  # Collect static files
+python3.11 manage.py migrate --noinput  # Run migrations
 
 echo "BUILD END"
