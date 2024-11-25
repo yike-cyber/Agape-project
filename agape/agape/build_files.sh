aadd6caf1d07945477ea4e2dafb86c33400b4d2a
= <=>
2024-11-25 15:33:22 +0300

    redeploy

--- agape/agape/build_files.sh
+++ agape/agape/build_files.sh
@@ -1,4 +1,11 @@
- echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#!/bin/bash
+
+echo "BUILD START"
+
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.py collectstatic --noinput --clear
+
+python3.11 manage.py migrate --noinput
+
+
+echo "BUILD END"
