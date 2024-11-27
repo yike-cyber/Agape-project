347154eb38fff3d97d873f8a40a114dab3da6a45
= <=>
2024-11-27 11:48:22 +0300

    update userAdmin and add display profile_image

--- agape/api/admin.py
+++ agape/api/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.html import mark_safe
 from .models import User, Warrant, DisabilityRecord
+from django.conf import settings
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ['first_name', 'last_name', 'email', 'role', 'profile_image', 'is_staff']
+    list_display = ['first_name', 'last_name', 'email', 'role', 'profile_image_display', 'is_staff']
     search_fields = ['first_name', 'last_name', 'email', 'phone_number']
     ordering = ['first_name']
 
-    # Customize the fieldsets and add_fieldsets to use email as the identifier
+    # Customize the fieldsets to add the profile_image field
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number', 'profile_image')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'middle_name', 'phone_number', 'profile_image')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'role')}),
         ('Important dates', {'fields': ('last_login',)}),
     )
@@ -19,12 +21,15 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password', 'first_name', 'last_name', 'role', 'is_active', 'is_staff')}
+            'fields': ('email', 'password', 'first_name', 'last_name', 'middle_name', 'role', 'is_active', 'is_staff')}
         ),
     )
-    
-    def get_search_results(self, request, queryset, search_term):
-        return queryset.filter(email__icontains=search_term), False
+
+    # Custom method to display profile image
+    def profile_image_display(self, obj):
+        return mark_safe(f'<img src="{obj.profile_image.url}" width="50" height="50" />') if obj.profile_image else mark_safe(f'<img src="{settings.MEDIA_URL}default_profile_image/avatar.png" width="50" height="50" />')
+
+    profile_image_display.short_description = 'Profile Image'
 
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(Warrant)
